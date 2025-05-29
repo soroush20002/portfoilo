31068918e2716f69353fa4eec1c7acf813a76ff3
soroushnassaj <soroushnj5000@gmail.com>
2025-05-29 21:34:36 +0430

    FC

--- src/constants/index.js
+++ src/constants/index.js
@@ -87,75 +87,7 @@ const navItems = [
     // },
   ];
   
-  const slides = [
-    {
-      id: 1,
-      title: "Sofi",
-      img: "/images/p1.png",
-    },
-    {
-      id: 2,
-      title: "Jasmina",
-      img: "/images/p2.png",
-    },
-    {
-      id: 3,
-      title: "d.tampe",
-      img: "/images/p3.png",
-    },
-    {
-      id: 4,
-      title: "Blimp.gr",
-      img: "/images/p4.png",
-    },
-    {
-      id: 5,
-      title: "Hawk Style Design",
-      img: "/images/p5.png",
-    },
-    {
-      id: 6,
-      title: "Lewis",
-      img: "/images/p6.png",
-    },
-    {
-      id: 7,
-      title: "Sofi",
-      img: "/images/p1.png",
-    },
-  ];
-  
-  const testimonials = [
-    {
-      name: "John Miller",
-      pos: "Founder of ModernEdge Solutions",
-      review:
-        "David Jhon turned our vision into a stunning, functional platform that our customers love. Their creativity and technical expertise truly set them apart.",
-      imgPath: "/images/client1.png",
-    },
-    {
-      name: "Emily Carter",
-      pos: "UX Designer at PixelWorks Studio",
-      review:
-        "David Jhon consistently brings fresh ideas and innovative solutions. Their passion for creativity and attention to detail elevate every project.",
-      imgPath: "/images/client2.png",
-    },
-    {
-      name: "Sarah Lopez",
-      pos: "Entrepreneur and Small Business Owner",
-      review:
-        "Exceeded my expectations with a unique and beautifully designed product that works flawlessly. Their creative touch is outstanding.",
-      imgPath: "/images/client3.png",
-    },
-    {
-      name: "David Chen",
-      pos: "Project Manager at CreativeSphere Agency",
-      review:
-        "Blends technical skills with bold creativity to deliver exceptional results. They push boundaries and elevate every project they work on.",
-      imgPath: "/images/client4.png",
-    },
-  ];
-  
+
   const footerIconsList = [
     {
       name: "Facebook",
@@ -183,7 +115,5 @@ const navItems = [
     navItems,
     bentoSocialLinks,
     iconsList,
-    slides,
-    testimonials,
     footerIconsList,
   };
